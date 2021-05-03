96aa6cfe602efa6e7097072c9207350a42aca0e6
restuccia <mario@restuccia.re>
2021-05-03 11:27:26 +0200

    Changed the order of the measures for the pdf script

--- doc_pdf/main.py
+++ doc_pdf/main.py
@@ -12,14 +12,16 @@ md_folder = os.path.join(root_folder, 'mesures')
 doc_pdf_folder = os.path.join(root_folder, 'doc_pdf')
 
 themes_str_dict = {
-    '1-transition-ecologique' : 'Transition écologique',
-    '2-economie-emploi' : 'Économie - Emploi',
-    '3-sante-famille-handicap' : 'Santé - Famille - Handicap',
-    '4-education' : 'Éducation',
+    '1-education-jeunesse' : 'Éducation - jeunesse',
+    '2-economie-emploi' : 'Économie - emploi',
+    '3-transition-ecologique' : 'Transition écologique',
+    '4-agriculture' : 'Agriculture',
     '5-securite' : 'Sécurité',
-    '6-culture' : 'Culture',
-    '7-services-publics-territoires' : 'Services publics - Territoires',
-    '8-logement' : 'Logement'
+    '6-justice' : 'Justice',
+    '7-sante-famille-handicap' : 'Santé - famille - handicap',
+    '8-logement' : 'Logement',
+    '9-services-publics-territoires' : 'Services publics et territoires',
+    '10-culture' : 'Culture'
 }
 
 def emojify(string):
